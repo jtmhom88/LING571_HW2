d31364c868af05db14f83f6b7f3993b75b5c8809
johnny.hom <johnnyhom@gmail.com>
2013-02-01 23:20:32 -0800

    parses all sentences from inputfile.txt using grammar.cnf

--- jh_cky.py
+++ jh_cky.py
@@ -2,64 +2,123 @@
 
 import collections
 import ast
+import nltk
 from nltk.tree import *
 from nltk.draw import tree
  
+class Pair:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def printpair(self):
+        print self.x
+        print self.y
+
 def build(CNF):
     G = collections.defaultdict(list)
     for left, right in CNF:
         G[right].append(left)
-    print G
+    #print G
     return G
+
+def addleaves(Tree, ltree, rtree ):
+    Tree.append(ltree)
+    Tree.append(rtree)
+    return Tree
+    pass
  
 def cky(G, W):
-    Table = [[[] for j in range(len(W)+1)] for i in range(len(W))]
-    Trees = [[[] for j in range(len(W)+1)] for i in range(len(W))]
+    # cell of table is a list of pairs (tag, tree)
+    blankpair = Pair('', Tree('',[]))
+    Table = [[ [blankpair] for j in range(len(W)+1)] for i in range(len(W))]
     for j in range(1, len(W)+1):
         theword = W[j-1]
         taglist = G.get(theword, [])
-        Table[j-1][j] += taglist
+        # iterate the tags, and make a pair entry for each
         for tag in taglist:
             tagtree = Tree(tag, [theword])
-            Trees[j-1][j].append(tagtree)
-            print "Tree %r" % (tagtree)
-            print "Treej-1,j [%d,%d]: %r" % (j-1,j,Trees[j-1][j])
-        #print "j-1,j[%d,%d]: %s %r" % (j-1, j, W[j-1], G.get(W[j-1]))
-        print "Tj-1,j [%d,%d]=%r" % (j-1,j,Table[j-1][j])
+            thispair = Pair(tag, tagtree)
+            #thispair.printpair()
+            nowcell = Table[j-1][j]
+            #print nowcell
+            Table[j-1][j].append(thispair)
+        for e in Table[j-1][j]:
+            #print "[%d,%d]: %r %r" % (j-1, j, e.x, e.y)
+            pass
+
         for i in range(j-2, -1, -1):
-            #print "i %d" % i
             for k in range(i+1, j):
-                print "k %d i %d j %d " % (k,i,j)
-                for x in Table[i][k]:
-                    #print "Ti,k [%d,%d]=%r" % (i,k,Table[i][k])
-                    for y in Table[k][j]:
-                        #print "Tk,j [%d,%d]=%r" % (k,j,Table[k][j])
-                        Table[i][j] += G.get((x,y), [])
-                        print "i,j[%d,%d]: %r (%s [%d,%d] and %s [%d,%d])" % (
-                            i,j,G.get((x,y)),x,i,k,y,k,j)
-                        """
-                        print i
-                        print j
-                        print x,y,"-->",G.get((x,y),[])
-                        print x
-                        print i
-                        print k
-                        print y
-                        print k
-                        print j
-                        """
-                        print "Ti,j[%d,%d]: %r " % (i,j,Table[i][j])
-    print "FINAL Table[0][%d]=%r" % (len(W),Table[0][len(W)])
-    return Table
+                for e in Table[i][k]:
+                    for f in Table[k][j]:
+                        #e.printpair()
+                        #f.printpair()
+                        # each elem of Table is a pair
+                        taglist = G.get((e.x,f.x), [])
+                        #print "taglist e.x f.x %r %r %r" % (taglist,e.x,f.x)
+                        for tag in taglist:
+                            tagtree = Tree(tag, [])
+                            tagtree.append(e.y)
+                            tagtree.append(f.y)
+                            #print "tagtree %r" % (tagtree)
+                            thispair = Pair(tag, tagtree)
+                            #thispair.printpair()
+                            nowcell = Table[i][j]
+                            #print "nowcell %r" % nowcell
+                            Table[i][j].append(thispair)
+                        for ee in Table[i][j]:
+                            #print "[%d,%d]: %r %r" % (i, j, ee.x, ee.y)
+                            pass
+    # Final Answer is in upper right cell 
+    # cell is list of pairs
+    outstring = ''
+    finalcell = Table[0][len(W)]
+    #print "\nFINAL PARSE TREES:"
+    n = 0
+    for e in finalcell:
+        # each element e is a pair
+        finaltag = e.x
+        finaltree = Tree('TOP', [e.y])
+        if (e.x == 'S' or e.x == 'SQ' or e.x == 'FRAG'):
+            #print "FINAL tag: %s" % finaltag
+            #print "FINAL tree %d:\n%s" % (n,finaltree)
+            #print "\n"
+            outstring += "\nFINAL tree %d:\n%s\n" % (n,finaltree)
+            n += 1
+
+    return (outstring)
+ 
  
 if __name__ == '__main__':
-    #grammarfile = 'grammar.cfg'
-    grammarfile = 'julie.cnf'
+    outfile = open('hw2.out','w')
+    grammarfile = 'grammar.cnf'
+    #grammarfile = 'julie.cnf'
     linestring = open(grammarfile, 'r').read()
     CNF = ast.literal_eval(linestring)
 
     G = build(CNF)
-    T = cky(G, ('book', 'the', 'flight', 'through', 'Houston'))
+    #T = cky(G, ('book', 'the', 'flight', 'through', 'Houston'))
     #T = cky(G, ('work', 'accelerates', 'the', 'growth', 'of', 'muscles'))
+    #T = cky(G, ('muscles', 'grow', 'in', 'response', 'to', 'work'))
+
+    # Iterate through all the files in inputfile.txt
+    i = 0
+    temp = tok = ll = []
+    infile = open('inputfile.txt','r')
+    for line in infile:
+        temp = line.strip()
+        ll.append(temp)
+        tok = nltk.word_tokenize(temp)
+        T = cky(G,tok)
+        outfile.write("-----------------------------\n")
+        outfile.write("Line %d: %s" % (i,line))
+        outfile.write(T)
+        #print "-----------------------------"
+        #print "Line %d: %s" % (i,line)
+        #print T
+        #print "\n"
+        #if i> 4:
+        #    break
+        i += 1
+        pass
 
 
